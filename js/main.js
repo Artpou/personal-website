b28ead4c9bfac4296e99fddd6c9e179613181eca
Artpou <arthur.poullin99@gmail.com>
2020-06-15 03:15:47 +0200

    new version for Hetic

--- js/main.js
+++ js/main.js
@@ -201,38 +201,6 @@
 	};
 	contentWayPoint();
 
-	// magnific popup
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
-    }
-  });
-
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-  });
-
-
   var goHere = function() {
 
 		$('.mouse-icon').on('click', function(event){
@@ -249,8 +217,6 @@
 	goHere();
 
 	// $("#myScrollspy").scrollspy({ offset: -75 });
-
-
 
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
@@ -314,11 +280,6 @@ window.onload = function() {
 
 
 })(jQuery);
-
-
-
-
-
 
 
 // this makes the height of each page equal to the height of the window
